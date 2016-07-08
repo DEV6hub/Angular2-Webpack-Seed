86d83dd57116d5531c3196e8e1857e7ffb2f0d77
Andrew Smith <andrews@dev6.com>
2016-07-08 09:31:13 -0400

    Add new @angular/forms module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 import { AppComponent } from './app/';
 
@@ -9,4 +10,7 @@ if (ENV === 'production') {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  disableDeprecatedForms(),
+  provideForms()
+]);
